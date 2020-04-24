0465a77a0c7f98804c4ae605f68b29d8874f949f
Victor Plushko <vicplushko33@gmail.com>
2020-04-24 02:46:53 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -7,9 +7,13 @@ class Person
 
   def get_married(person)
     self.partner = person
+    if person.class != Person
+      raise PartnerError
+    else
     person.partner = self
   end
-
+class PartnerError < StandardError
+  end
 end
 
 beyonce = Person.new("Beyonce")
